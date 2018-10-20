68fd315481897a5bd0b325f7e9ef3ccd0374ecc0
Termeni <joseppascualbadia@gmail.com>
2018-10-20 03:11:21 +0200

    Login activity - Setup

--- app/src/main/java/tech/owly/owly/MainActivity.kt
+++ app/src/main/java/tech/owly/owly/MainActivity.kt
@@ -2,11 +2,28 @@ package tech.owly.owly
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sign_in.setOnClickListener {
+            val username = username.text.toString()
+            val password = password.text.toString()
+            if (validateCredentials(username,password)){
+                Toast.makeText(this,"Login successful!",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                Toast.makeText(this,"Invalid credentials",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+    fun validateCredentials(username: String, password: String) =
+        username == "admin" && password == "admin"
+
 }
